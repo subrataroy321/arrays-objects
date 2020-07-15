69a840cda8e4bb2116ef7893e689f235ea33d7e2
SUBRATA ROY <subrata.r321@gmail.com>
2020-07-15 17:30:47 -0400

    [while loop] add

--- app.js
+++ app.js
@@ -8,4 +8,39 @@ var favoriteNumber = 5;
 console.log(favoriteNumber);
 
 var teams = ['Rockets','Lakers', 'Warriors', 'clippers', 'blazzers', 'Celtics','Nets','Falcons'];
+
 console.log(teams);
+
+
+//while loops
+var number = 10;
+var otherNumber = 1;
+while (otherNumber < number) {
+    console.log(otherNumber);
+    otherNumber += 1;
+
+}
+
+var numberTwo = 20;
+var myOtherNumber = 10;
+
+while (myOtherNumber < numberTwo) {
+    console.log(myOtherNumber);
+    myOtherNumber += 1;
+}
+
+var numberThree = 30;
+var otherNumberTwo = 20;
+
+while (otherNumberTwo < numberThree) {
+    console.log(otherNumberTwo);
+    otherNumberTwo += 1;
+}
+
+var numberFour = 40;
+var otherNumberThree = 30;
+
+while (otherNumberThree < numberFour) {
+    console.log(otherNumberThree);
+    otherNumberThree += 1;
+}
