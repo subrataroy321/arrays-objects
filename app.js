95ba035db0f0152d21993f422f55fde04dd11b54
SUBRATA ROY <subrata.r321@gmail.com>
2020-07-15 17:04:35 -0400

    [Arrays] add favorite teams

---
+++ app.js
@@ -0,0 +1,11 @@
+var name = "Subrata";
+console.log(name);
+
+var myLocation = 'Atlanta';
+console.log(myLocation);
+
+var favoriteNumber = 5;
+console.log(favoriteNumber);
+
+var teams = ['Rockets','Lakers', 'Warriors', 'clippers', 'blazzers', 'Celtics','Nets','Falcons'];
+console.log(teams);
